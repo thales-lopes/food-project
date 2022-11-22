47335b5698d283ed29914311055448192693de19
thales-lopes <thaaalees@outlook.com>
2022-11-21 23:27:10 -0300

    Background Style Changes Added & Images Carousel also addes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-
+$('.slider-pictures').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: false,
+    autoplaySpeed: 2000
+  });
